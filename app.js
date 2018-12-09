405aa09c1f3f764cf057dc384834859d94ba9fc8
Jose Collas <joseherminiocollas@goatstone.com>
2018-12-08 16:28:03 -0800

    add currency converter

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 let atmRouter = require('./routes/atm')
+let currConvRouter = require('./routes/curr-conv')
 
 var app = express();
 
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/atm', atmRouter);
+app.use('/curr-conv', currConvRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
