eb556b2ea763342d4890379b396978170aaa6279
Yashar Fakhari <YasharF.Git1@ydftech.com>
2018-12-08 17:14:22 -0800

    Add /restaurants

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 let atmRouter = require('./routes/atm')
+let restaurantsRouter = require('./routes/restaurants');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/atm', atmRouter);
+app.use('/restaurants', restaurantsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
