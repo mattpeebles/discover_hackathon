da324445e26d0ec85a99b97f816327fca33d845f
Matt Peebles <peebles.mm@gmail.com>
2018-12-08 18:30:45 -0800

    Added map to ui

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index');
 });
+
+router.get('/openMapGl', (req, res, next) => {
+  res.send(config.openMapKey);
+})
 
 router.post('/basic', (req, res, nex) => {
   console.log(req);
